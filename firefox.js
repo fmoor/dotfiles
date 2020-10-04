9521df0af6e88b347635657ffda26e5fe9122657
fmoor <fmoor@gmx.com>
2020-10-04 13:27:45 -0700

    set download directory

--- firefox.js
+++ firefox.js
@@ -1,2 +1,6 @@
 user_pref("signon.rememberSignons", false);
 user_pref("browser.tabs.drawInTitlebar", true);
+user_pref("browser.download.dir", "/tmp");
+user_pref("browser.download.defaultFolder", "/tmp/");
+user_pref("browser.download.folderList", 2);
+user_pref("browser.download.useDownloadDir", true)
