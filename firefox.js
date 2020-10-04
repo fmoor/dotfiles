e354b4563d0d612cdeed791b40406a34e6ef05d1
fmoor <fmoor@gmx.com>
2020-10-04 15:07:03 -0700

    change ctrl+tab behavior

--- firefox.js
+++ firefox.js
@@ -3,4 +3,5 @@ user_pref("browser.tabs.drawInTitlebar", true);
 user_pref("browser.download.dir", "/tmp");
 user_pref("browser.download.defaultFolder", "/tmp/");
 user_pref("browser.download.folderList", 2);
-user_pref("browser.download.useDownloadDir", true)
+user_pref("browser.download.useDownloadDir", true);
+user_pref("browser.ctrlTab.recentlyUsedOrder", false);
